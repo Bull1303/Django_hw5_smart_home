2c6a4ac0afc09bb11b677a97f7091ad69fa06415
Bull1303 <2lp2@mail.ru>
2023-10-26 22:00:12 +0300

    Update

--- measurement/tests.py
+++ measurement/tests.py
@@ -2,10 +2,10 @@
 from unittest import TestCase
 from rest_framework.test import APIClient
 
+
 class TestSmth(TestCase):
-    def test_sample_view_ok (self):
+    def test_sample_view_ok(self):
         client = APIClient()
         url = '/api/sensors/'
         response = client.get(url)
         self.assertEqual(response.status_code, 200)
-
